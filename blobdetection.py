224ef22b563e61968dcb10f1c306f24aed65d4b9
Huayin Luo <hclover2003@gmail.com>
2023-03-13 21:00:46 -0400

    full image

--- blobdetection.py
+++ blobdetection.py
@@ -21,7 +21,7 @@ def get_frame_coords(frame: int, neuron_allframes: List) -> List:
     """
     return (np.ceil(neuron_allframes[frame,:])).astype(int)
 
-def crop_image(imgs: List, frame: int, channel: int, position: Tuple[int, int], crop_size: Tuple[int, int]) -> List:
+def crop_image(imgs: List, frame: int, channel: int, position: Tuple[int, int], crop_size: int) -> List:
     """ Returns cropped grayscale image
 
     Parameters:
@@ -32,12 +32,12 @@ def crop_image(imgs: List, frame: int, channel: int, position: Tuple[int, int], 
     Returns:
     img: image in two dimension array (width, height)
     """
-    dimensions = imgs[frame, channel, :, :].shape # image dimensionss
+    (img_y, img_x) = imgs[frame, channel, :, :].shape # image dimensionss
     # avoid cropping out of bounds
     img = imgs[frame,
                 channel,
-                max(position[1]-crop_size[1], 0):min(position[1]+crop_size[1], dimensions[1]), # height (top:bottom)
-                max(position[0]-crop_size[0], 0):min(position[0]+crop_size[0], dimensions[0]) # width (left:right)
+                max(position[1]-crop_size, 0):min(position[1]+crop_size+1, img_y), # height (top:bottom)
+                max(position[0]-crop_size, 0):min(position[0]+crop_size+1, img_x) # width (left:right)
                 ]
     return img
 
@@ -75,41 +75,44 @@ def draw_circle(img: List, thres_img: List, position: List) -> List:
                                             )
     
     return im_with_keypoints
-
-def contour_detection(img, thres_img, min_area):
-    contours = cv2.findContours(thres_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
-    line_length = 20
-    for c in contours:
-        if cv2.contourArea(c) >= min_area:
-            M = cv2.moments(c)
-            x = int(M['m10']/M['m00'])
-            y = int(M['m01']/M['m00']) 
-            cv2.line(img, (x-line_length, y), (x+line_length, y), (255, 0, 0), 2)
-            cv2.line(img, (x, y-line_length), (x, y+line_length), (255, 0, 0), 2)
-    return img
             
 # Load Data
 imgs = np.load("11408_crop.nd2.npy")
 ava = np.load("AVA_11408.mat.npy")
 avb = np.load("AVB_11408.mat.npy")
 
-frames = 20 # frames = imgs.shape[0] to run for all frames
+start_frame = 0 #imgs.shape[0]
+end_frame = 201
+print(f"{end_frame-start_frame} frames")
 channel = 0
-crop_size = (10,10)
-chosen_av = avb
-crop = False # to skip cropping, set to False
+(img_y, img_x) = imgs[0, 0, :, :].shape
+
+crop_size = 10
+thres = 50
+
+crop = True # to skip cropping, set to False
 blob = True # to skip blob detection, set to False
+save = True # to save as npy file, set to True
 
 # Crop Images
 if crop:
     print("Start Cropping")
     try:
-        for frame in range(frames):
-            position = get_frame_coords(frame, chosen_av)
-            img = crop_image(imgs, frame, channel, position, crop_size) # crop image
-            plt.imsave(f'{file_dir}/cropped_imgs/cropped_{frame}.png', img, cmap="gray") # save cropped image
+        for frame in range(start_frame, end_frame):
+            # save original image (to check manually against final product)
+            plt.imsave(f'{file_dir}/og_imgs/{frame}.png', imgs[frame,0,:,:], cmap="gray")
+
+            # neuron positions
+            position_a = get_frame_coords(frame, ava)
+            position_b= get_frame_coords(frame, avb)
             
-            print(f"position: {position}")
+            # crop image & save
+            img_a = crop_image(imgs, frame, channel, position_a, crop_size)
+            img_b = crop_image(imgs, frame, channel, position_b, crop_size)
+            
+            plt.imsave(f'{file_dir}/cropped_imgs/{frame}a.png', img_a, cmap="gray")
+            plt.imsave(f'{file_dir}/cropped_imgs/{frame}b.png', img_b, cmap="gray") 
+                   
             print(f"------- cropped frame {frame}-------")
     except Exception as e:
         print(f"error cropping frame {frame}")
@@ -121,22 +124,37 @@ if crop:
 # Blob Detection
 if blob:
     print("Start Blob Detection")
-    for frame in range(frames):
-        try:         
-            img_gray = cv2.imread(f"{file_dir}/cropped_imgs/cropped_{frame}.png") # read image
-            # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
+    npy_imgs=[]
+    for frame in range(start_frame, end_frame):
+        # Read images        
+        img_gray_a = cv2.imread(f"{file_dir}/cropped_imgs/{frame}a.png")
+        img_gray_b = cv2.imread(f"{file_dir}/cropped_imgs/{frame}b.png") 
+        
+        # Threshold image
+        ret_a, thres_img_a = cv2.threshold(img_gray_a, thres, 255, cv2.THRESH_BINARY)
+        ret_b, thres_img_b = cv2.threshold(img_gray_b, thres, 255, cv2.THRESH_BINARY)     
+        
+        # Save threshold images
+        plt.imsave(f'{file_dir}/thres_imgs/{frame}a.png', thres_img_a) # save threshold image
+        plt.imsave(f'{file_dir}/thres_imgs/{frame}b.png', thres_img_b)
+        
+        full_img = np.zeros((img_y, img_x, 3))
+        (xa, ya) = get_frame_coords(frame, ava)
+        (xb, yb) = get_frame_coords(frame, avb)
+        # Paste into original image
+        for i in range(thres_img_a.shape[0]):            
+            row_a = full_img[min(max(ya-crop_size + i, 0), img_y)]
+            row_b = full_img[min(max(yb-crop_size + i, 0), img_y)]
             
-            ret, thres_img = cv2.threshold(img_gray, 60, 255, cv2.THRESH_BINARY_INV) # threshold & invert image
-            plt.imsave(f'{file_dir}/thres_imgs/thres_img{frame}.png', thres_img) # save threshold image
+            row_a[max(xa-crop_size, 0):min(xa+crop_size+1, img_x)] = thres_img_a[i]
+            row_b[max(xb-crop_size, 0):min(xb+crop_size+1, img_x)] = thres_img_b[i]
             
-            blob_img = draw_circle(img_gray, thres_img, get_frame_coords(frame, chosen_av)) # use blob detection
-            
-            # blob_img = contour_detection(img, img_gray, 20) # use contour detection (doesn't work yet, incompatible array)
-            
-            plt.imsave(f'{file_dir}/blob_imgs/blob_img{frame}.png', blob_img) # save blob image
-            
-            print(f"-------finished frame {frame}-------")
-        except:
-            print(f"error blob detection frame {frame}")
-            pass           
+        norm_full_img = cv2.normalize(src=full_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        plt.imsave(f'{file_dir}/full_imgs/{frame}.png', norm_full_img )
+        npy_imgs.append(norm_full_img)
+        print(f"-------finished frame {frame}-------")
+    if save:
+        imgset=np.array(npy_imgs)
+        np.save("imgds.npy",imgset)       
     print("Finished!")
+    
